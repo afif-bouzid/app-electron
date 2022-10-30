5ee3f38f495d21e9f0f752187e261413f157cdf0
afif bouzid <afif@afif.com>
2022-10-30 21:48:19 +0100

    Initial commit

---
+++ main.js
@@ -0,0 +1,19 @@
+const { app, BrowserWindow } = require('electron')
+const path = require('path')
+const { ipcMain } = require('electron')
+
+const createWindow = () => {
+    const win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            preload: path.join(__dirname, 'preload.js'),
+        },
+    })
+    ipcMain.handle('ping', () => 'pong')
+    win.loadFile('index.html')
+}
+
+app.whenReady().then(() => {
+    createWindow()
+})
